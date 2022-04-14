9a2608c61fb333d2fdaa1824f3431399efc01969
MoaresOliveira <moaresoliveira@gmail.com>
2022-04-14 16:03:50 -0300

    API Rick and Morty Consumer

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const thumbContainer = document.querySelector("body .container");
+const blurDiv = document.querySelector("body .blurDiv");
+const card = document.querySelector("body .card");
+const cardName = card.querySelector(".name");
+const cardImage = card.querySelector(".profile");
+const cardInfoStatus = card.querySelector(".info .status");
+const cardInfoSpecies = card.querySelector(".info .species");
+const cardInfoGender = card.querySelector(".info .gender");
+const cardInfoOrigin = card.querySelector(".info .origin span");
+let next = "";
+
+document.body.addEventListener('click', ev => {
+    card.style.top = `${ev.clientY} !important`;
+    card.style.left = `${ev.clientX } !important`;
+})
+
+async function fetchAPI(page = null){
+    let reponse = await fetch(page ==null? "https://rickandmortyapi.com/api/character" : page);
+    let objectJson = reponse.json();
+    objectJson.then(json =>{
+        thumbGenerator(json.results);
+        next = json.info.next;
+    });
+}
+
+async function fetchCharacter(characterUrl){
+    let url = characterUrl.replaceAll(" ", "/")
+    let reponse = await fetch(url);
+    let objectJson = reponse.json();
+    objectJson.then(json =>{
+        showCard(json);
+    });
+}
+
+function loadMore(){
+    fetchAPI(next);
+}
+
+function thumbGenerator(characterList){
+    for(let i = 0; i < characterList.length; i++){
+        let item = `<div class="item" onclick="fetchCharacter('${characterList[i].url}')">
+                        <img src="${characterList[i].image}" width="60" alt="Character ${characterList[i].name} from the show Rick and Morty" class="thumb">
+                        <h3 class="name">${characterList[i].name}</h3>
+                    </div> `
+        thumbContainer.innerHTML += item;
+    }
+}
+
+function showCard(json){
+    cardName.textContent = json.name;
+    cardImage.setAttribute('src', json.image);
+    cardInfoStatus.style.color = json.status == 'Alive'? '#0ff': json.status == 'Dead'? '#b00': '#ccc';
+    cardInfoStatus.textContent = json.status;
+    cardInfoSpecies.textContent = json.species;
+    cardInfoGender.textContent = json.gender;
+    cardInfoOrigin.textContent = json.origin.name;
+    console.log(card.style)
+    card.classList.remove("hidden")
+    blurDiv.classList.remove("hidden")
+}
+
+function hide(){
+    card.classList.add("hidden")
+    blurDiv.classList.add("hidden")
+}
